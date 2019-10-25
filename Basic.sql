7214ce53d6b679169f9840c4a73ba9aeea67f0a3
Yourong Ye <yourong.ye@hotmail.com>
2019-10-25 18:31:15 +0100

    Update Basic.sql

--- Basic.sql
+++ Basic.sql
@@ -1,3 +1,20 @@
+-- create table with diff data types
+CREATE TABLE Staff
+(
+  staffID INTEGER AUTO_INCREMENT PRIMARY KEY,
+  itemid INTEGER,
+  startDate DATETIME NOT NULL,
+  Salary DECIMAL,
+  isRetired bit NULL DEFAULT 0, -- SQL没有bool值，只有bit
+  FOREIGN KEY (propertyID) REFERENCES Property (propertyID)
+ );
+ 
+-- general query statement
 SELECT *
 FROM Auction
 WHERE ENDDATE<=CURRENT_TIMESTAMP
+
+-- 如果要修改某个表里的某些值，可以用
+UPDATE Auction
+    SET isFinished = 1
+   WHERE Auction.endDate >= CURRENT_TIMESTAMP;
