be691f3fa7f9b61bd0dfe1ea7259e599ebdffd53
Yourong Ye <yourong.ye@hotmail.com>
2019-11-10 16:31:36 +0000

    Update lecture_notes.sql

--- lecture_notes.sql
+++ lecture_notes.sql
@@ -1,7 +1,12 @@
+-- Select everything in the table.
 SELECT * FROM table_name;
+
+-- Select columns in the table.
 SELECT column1, column2
 FROM table_name;
 
 -- Select all the different values from the Country column in the Customers table.
-SELECT DISTINCT column_name
-FROM table;
+SELECT DISTINCT Country -- country name不会重复
+FROM Customers;
+
+
