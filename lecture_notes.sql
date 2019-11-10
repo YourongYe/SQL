1a131734d84588803c20d00355f0d68836a33e12
Yourong Ye <yourong.ye@hotmail.com>
2019-11-10 16:39:52 +0000

    Update lecture_notes.sql

--- lecture_notes.sql
+++ lecture_notes.sql
@@ -27,3 +27,8 @@ SELECT * FROM Customers
 WHERE City = "Berilin"
 AND PostalCode = 12209
 
+-- Select all records where the City column has the value 'Berlin' or 'London'.
+SELECT * FROM Customers
+WHERE City = 'Berlin'
+OR CITY = "London"; 
+
