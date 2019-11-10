8b0785c84a4d1683b15af9950d0db4441eb09180
Yourong Ye <yourong.ye@hotmail.com>
2019-11-10 16:38:28 +0000

    Update lecture_notes.sql

--- lecture_notes.sql
+++ lecture_notes.sql
@@ -1,3 +1,4 @@
+--------------------****** SELECT ******--------------------
 -- Select everything in the table.
 SELECT * FROM table_name;
 
@@ -9,4 +10,20 @@ FROM table_name;
 SELECT DISTINCT Country -- country name不会重复
 FROM Customers;
 
+-- Select all records where the City column has the value "Berlin".
+SELECT * FROM Customers
+WHERE City = "Berlin";
+
+-- Use the NOT keyword to select all records where City is NOT "Berlin".
+SELECT * FROM Customers
+WHERE NOT City = "Berilin"
+
+-- Select all records where the CustomerID column has the value 32.
+SELECT * FROM Customers
+WHERE CustomerID = 32;
+
+-- Select all records where the City column has the value 'Berlin' and the PostalCode column has the value 12209.
+SELECT * FROM Customers
+WHERE City = "Berilin"
+AND PostalCode = 12209
 
