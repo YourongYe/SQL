f801402c5147e243e18f3e3a9bd4f86d006115dc
Yourong Ye <yourong.ye@hotmail.com>
2019-11-10 16:29:54 +0000

    Create lecture_notes.sql

---
+++ lecture_notes.sql
@@ -0,0 +1,7 @@
+SELECT * FROM table_name;
+SELECT column1, column2
+FROM table_name;
+
+-- Select all the different values from the Country column in the Customers table.
+SELECT DISTINCT column_name
+FROM table;
