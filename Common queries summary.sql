6d7efe4994dc212995a09163b3efca76885c8fa3
Yourong Ye <yourong.ye@hotmail.com>
2019-11-23 20:15:03 +0000

    Update Common queries summary.sql

--- Common queries summary.sql
+++ Common queries summary.sql
@@ -30,3 +30,19 @@ select (case when g.grade>=8 then s.name else null end), grade, marks -- case wh
 from students s, grades g
 where marks between min_mark and max_mark -- 类似于where s.id = d.id, 但这里不是直接对应关系，而是需要对应到相应的区间
 order by g.grade desc, name asc
+
+
+/* 有4张表*/
+SELECT h.hacker_id, h.name
+FROM submissions s
+JOIN challenges c
+    ON s.challenge_id = c.challenge_id -- 这里的等号左右的位置可以调换，不会影响
+JOIN difficulty d
+    ON c.difficulty_level = d.difficulty_level -- 注意这里的两个column都不是来自submission table，只要跟其他任意一个table有连接就可以
+JOIN hackers h
+    ON s.hacker_id = h.hacker_id
+WHERE s.score = d.score  -- join table之后的限制条件
+    AND c.difficulty_level = d.difficulty_level
+GROUP BY h.hacker_id, h.name  -- group by 后面跟的column name必须跟select保持一致，如果这里只有一个h.hacker_id就会报错，因为h.name不会自动跟着一起group
+    HAVING COUNT(s.hacker_id) > 1  -- group by 后面还可以进一步限制，根据group by的结果进行限制
+ORDER BY COUNT(s.hacker_id) DESC, s.hacker_id ASC
